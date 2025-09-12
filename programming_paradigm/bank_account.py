8dd40c22de44119cb1c610f0107c71a584a3df54
MburuHannah <hannahmburu897@gmail.com>
2025-09-12 10:05:56 +0300

    Added other features

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,28 +1,18 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.account_balance=account_balance
-    def deposit(self, amount):
-        if amount >0:
-            self.account_balance += amount
-            return self.account_balance
-        else:
-            print("Deposit amount must be positive.")
-            
-    def withdraw(self,amount):
+        self.account_balance = account_balance
 
-            if amount>self.account_balance:
-                print("Insufficient funds.")
-                return False
-            else:
-             self.account_balance-=amount
-            print(f"Withdrawal successful")
-            return True
-        
-    def display_balance(self):        
-        print(f"Current Balance:${self.account_balance}")
-        
-        
-    
-        
-        
-      
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return True, self.account_balance
+        return False, self.account_balance
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False, self.account_balance
+        self.account_balance -= amount
+        return True, self.account_balance
+
+    def display_balance(self):
+        return self.account_balance
