3efa9d8ddcad3d166682b5067fb514e82e95afa2
MburuHannah <hannahmburu897@gmail.com>
2025-09-19 10:20:05 +0300

    More OOP

---
+++ oop/main2.py
@@ -0,0 +1,18 @@
+from library_system import Book, Ebook, PrintBook, Library
+
+def main():
+    my_library = Library()
+
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = Ebook("Snow Crash", "Neal Stephenson", 500)
+    paper_book = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_book)
+
+    my_library.list_books()
+
+
+if __name__ == "__main__":
+    main()
